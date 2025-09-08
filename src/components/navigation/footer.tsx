efd5fe41f9b748501c472aa634c4ee2d775ea008
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-08 11:50:03 +0600

    i am fine 😵‍ 🥲

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Footer() {
     return(
-        <div>
+        <div className="max-w-[1170px]  justify-between m-auto font-normal mt-[30px] mb-5 relative px-[14px] sm:px-[30px]">
             <div>
                 <div>
                     <div>

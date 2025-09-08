8de2405b38b7613811150f693bfe1937b090cccb
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-08 17:49:46 +0600

    add the user

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return(
         <div>
             <div className="relative">
-                <div className="w-100% bg-sec">
+                <div className="w-[100%] bg-sec">
                     <Image src={school} alt="ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়" className="w-[100vw] h-[49vw] lg:h-[30vw] object-cover  " />
                 </div>
                 <div className="absolute top-2 left-2">

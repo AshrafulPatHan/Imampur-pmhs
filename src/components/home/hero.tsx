9c752184d41caf8329804d56f298cb9d6fd9372e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-08 13:26:58 +0600

    add the user

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <div>
             <div>
                 <div className="w-100% bg-sec">
-                    <Image src={school} alt="ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়" className="w-[100vw] h-[49vw] object-cover  " />
+                    <Image src={school} alt="ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়" className="w-[100vw] h-[49vw] lg:h-[30vw] object-cover  " />
                 </div>
                 <div>
                     <h1>ইমামপুর পল্লী মঙ্গল উচ্চ বিদ্যালয়</h1>
